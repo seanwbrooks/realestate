66ec2d60a790790294fcfaafc2b3c3537253f0fd
Sean <swbrooks87@gmail.com>
2020-06-21 17:20:36 -0400

    Initial commit

---
+++ deal.py
@@ -0,0 +1,6 @@
+import requests
+
+r = requests.get('GET https://www.googleapis.com/gmail/v1/users/userId/messages/id')
+
+
+
---
+++ quickstart.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+import email
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    results = service.users().messages().list(userId='me',q='label:personal').execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print('No messages found.')
+    else:
+        print('Listings:')
+        for message in messages:
+            contents = service.users().messages().get(userId='me',id=message['id'],format='full').execute()
+            with open('./output.txt', 'w') as outputfile:
+                print(contents)
+                outputfile.write(base64.urlsafe_b64decode(contents['raw'].encode('ASCII')))
+
+if __name__ == '__main__':
+    main()
